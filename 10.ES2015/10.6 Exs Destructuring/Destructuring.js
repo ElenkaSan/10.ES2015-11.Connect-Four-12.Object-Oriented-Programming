6448c55549979ded2c872ae0a238bcee5528296d
Elenka^_^San <75818489+ElenkaSan@users.noreply.github.com>
2021-03-05 12:27:14 -0500

    Update Destructuring.js

--- 10.ES2015/10.6 Exs Destructuring/Destructuring.js
+++ 10.ES2015/10.6 Exs Destructuring/Destructuring.js
@@ -114,15 +114,3 @@ raceResults(['Tom', 'Margaret', 'Allison', 'David', 'Pierre'])
     allOther: ["David", "Pierre"]
   }
 */
-
-function squareAndFindEvens1(numbers){
-  var squares = numbers.map(function(num){
-    return num ** 2;
-  });
-  var evens = squares.filter(function(square){
-    return square % 2 === 0;
-  });
-  return evens;
-}
-
-const squareAndFindEvens = ((numbers) => numbers.map((num) => num ** 2).filter((square) => square % 2 === 0));
